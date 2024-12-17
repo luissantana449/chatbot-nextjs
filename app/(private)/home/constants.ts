06f719a105ef1e2816358aa3d6f3afbd84f81dfc
Luis Santana <luissantana@dp.mt.gov.br>
2024-12-17 14:05:37 -0400

    Atualiza as rotas dos assistentes no arquivo de constantes

--- app/(private)/home/constants.ts
+++ app/(private)/home/constants.ts
@@ -3,10 +3,10 @@ import { ServicosTypes } from "./types";
 export const SERVICOS_IA: ServicosTypes[] = [
   {
     label: "Assistente Geral",
-    route: "/chat?agentId=IQ1DT2QQ62&aliasId=KAIAU0XZXI",
+    route: "/chat?agentId=UBFGGUXDFG&aliasId=51XVWS54NP",
   },
   {
     label: "Assistente de gabinete",
-    route: "/chat?agentId=UBFGGUXDFG&aliasId=51XVWS54NP",
+    route: "/chat?agentId=IQ1DT2QQ62&aliasId=OFRZMGF6LY",
   },
 ];
